9af866be9c933080feb2eec9b5383975667c6882
aleksandr88p <aleksandr88p@gmail.com>
2026-09-21 23:28:29 +0200

    refactor into funcs

--- main.py
+++ main.py
@@ -14,49 +14,44 @@ punctuation = ".,!?;:-()[]{}\"“”—–…«»‹›"
 
 file_name = "transcript.txt"
 
+def read_file(filename: str) -> str:
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+def normalize_content(content: str) -> str:
+    lower_content = content.lower()
+    clean_content = lower_content
+    for char in punctuation:
+        clean_content = clean_content.replace(char, " ")
+    return clean_content
+
+def remove_stop_words(words: list[str]) -> list[str]:
+    filtered_words = [word for word in words if word not in stop_words]
+    return filtered_words
+
 
 try:
-    with open(file_name, 'r', encoding='utf-8') as f:
-        content = f.read()
+    content = read_file(filename=file_name)
 except FileNotFoundError:
-    print(f"File {file_name} not found")
+    print("File not found")
     exit(1)
 
 
 if not content.strip():
     print("File is empty")
-    exit(0)
+    exit(1)
 
-lower_content = content.lower()
-
-
-clean_content = lower_content
-for char in punctuation:
-    clean_content = clean_content.replace(char, " ")
-
-clean_content_words_raw = clean_content.split()
-
-clean_content_words = [word for word in clean_content_words_raw if word not in stop_words]
-
-clean_dict = {}
-for word in clean_content_words:
-    if word in clean_dict:
-        clean_dict[word] += 1
-    else:
-        clean_dict[word] = 1
-
-
-sorted_clean_dict = dict(sorted(clean_dict.items(), key=lambda item: item[1], reverse=True))
-
-word_count = len(clean_content_words_raw)
-unique_words = len(sorted_clean_dict)
-top_30 = list(sorted_clean_dict.items())[:30]
-
-cnt = Counter(clean_content_words)
-
+clean_content = normalize_content(content)
+words = clean_content.split()
+filtered_words = remove_stop_words(words)
+word_count = len(words)
+unique_words = len(set(words))
+cnt = Counter(filtered_words)
 print("Total words:", word_count)
 print("Unique words:", unique_words)
-print("Top 30:", top_30)
 print()
 print("Top 30 (Counter):", cnt.most_common(30))
+
 
